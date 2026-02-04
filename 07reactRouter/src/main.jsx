fa7dbddc19cb43931e756d2f1a0e7e84a88f2483
lovepreet-singh-code <info.thelovepreetsingh@gmail.com>
2026-02-04 20:06:00 +0530

    React Router

---
+++ 07reactRouter/src/main.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+
+import Layout from "./Layout.jsx";
+import Home from "./components/Home/Home.jsx";
+import About from "./components/About/About.jsx";
+import Contact from "./components/Contact/Contact.jsx";
+import User from "./components/User/User.jsx";
+import Github, { githubInfoLoader } from "./components/Github/Github.jsx";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+
+      {/* ✅ Default Home Route */}
+      <Route index element={<Home />} />
+
+      {/* Other Routes */}
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="user/:userid" element={<User />} />
+
+      {/* Github Route with Loader */}
+      <Route
+        path="github"
+        element={<Github />}
+        loader={githubInfoLoader}
+      />
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
+
+
